ed1c4dfa22506568d6cad0dcea242de63b9788d7
Arthurpmrs <arthurpmrs@gmail.com>
2023-01-03 09:44:25 -0300

    Add getPlayerChoice function

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+const choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
     let chosen_index = Math.floor(3 * Math.random());
-    return choices[chosen_index]
+    return choices[chosen_index];
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice() {
+    let choice;
+    
+    while (true) {
+        choice = prompt("Enter your choice (Rock, paper or scissors): ");
+
+        if (choice == "" || choice == null) {
+            return null;
+        } else if (!(choices.includes(choice.toLowerCase()))) {
+            alert("Invalid entry. Try again!");
+        } else {
+            return choice.toLocaleLowerCase();
+        }
+    }
+
+}
